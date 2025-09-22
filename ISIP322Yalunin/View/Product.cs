f882fc170765f530653fb1d10bba380b80274255
227157 <maximya@bk.ru>
2025-09-22 10:37:45 +0300

    перенес структуру класса из MainWindow.cs в View->Product

--- ISIP322Yalunin/View/Product.cs
+++ ISIP322Yalunin/View/Product.cs
@@ -8,6 +8,21 @@ namespace ISIP322Yalunin.View
 {
     internal class Product
     {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public bool InStock { get; set; }
+        public string Category { get; set; }
 
+        public Product(string code, string name, decimal price, int quantity, string category)
+        {
+            Code = code;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            InStock = quantity > 0;
+            Category = category;
+        }
     }
 }
