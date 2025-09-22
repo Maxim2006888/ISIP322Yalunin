f04b5cd215fc8b337873bf5091ffdcce51dfc3b8
227157 <maximya@bk.ru>
2025-09-22 10:30:50 +0300

    прописал в классе продуктов, свойства рассматриваемого объекта

--- ISIP322Yalunin/MainWindow.xaml.cs
+++ ISIP322Yalunin/MainWindow.xaml.cs
@@ -22,6 +22,22 @@ namespace ISIP322Yalunin
 
         public class Product
         {
+            public string Code { get; set; }
+            public string Name { get; set; } 
+            public decimal Price { get; set; }
+            public int Quantity { get; set; }
+            public bool InStock { get; set; }
+            public string Category { get; set; }
+
+            public Product(string code, string name, decimal price, int quantity, string category)
+            {
+                Code = code;
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                InStock = quantity > 0;
+                Category = category;
+            }
 
         }
     }
