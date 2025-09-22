eaf13a6d1f8f02768e10ccfb4f60166bcd3ba907
227157 <maximya@bk.ru>
2025-09-22 10:46:32 +0300

    Создал класс для управления товарами

---
+++ ISIP322Yalunin/View/InventoryManager .cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISIP322Yalunin.View
+{
+    internal class InventoryManager
+    {
+        private List<Product> products = new List<Product>();
+        public void AddProduct(Product product)
+        {
+            products.Add(product);
+        }
+    }
+}
